9d47a851c2a82383a412ac8e4788a7af4929f44c
realsnipc <snipc.mail@proton.me>
2024-01-21 22:13:50 +0530

    update footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -10,7 +10,7 @@ export default function Footer() {
   <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
 
     <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-      <a href="https://github.com/realsnipc" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@snipc</a>
+      <a href="https://github.com/realsnipc" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank"> Made by Shaurya & AI</a>
     </p>
     <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start gap-2 ">
       <a href='https://x.com/realsnipc'><RiTwitterXLine /></a>  
